88cc73b8735e315d8d8d30756c0c110c5e9d775f
Hammad Asghar <ihammadasghar@gmail.com>
2025-05-17 23:36:21 +0100

    Added authentication to database

--- SportTeamManagementTool/api/models.py
+++ SportTeamManagementTool/api/models.py
@@ -1,15 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Athlete(User):
-    birth_date = models.DateField()
-    profile_picture = models.ImageField(upload_to='athletes/', null=True, blank=True)
-    height = models.IntegerField()
-
-    def __str__(self):
-        return f"Athlete: {self.username}"
-
-
 class Trainer(User):
     specialization = models.CharField(max_length=100, blank=True)
     profile_picture = models.ImageField(upload_to='trainers/', null=True, blank=True)
@@ -25,21 +16,31 @@ class Member(User):
     def __str__(self):
         return f"Member: {self.username}"
 
-class Team(models.model):
+class Team(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     logo = models.ImageField(upload_to='teams/', null=True, blank=True)
     members = models.ManyToManyField(Member, through='Membership')
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Team: {self.name}"
 
+class Athlete(User):
+    birth_date = models.DateField()
+    profile_picture = models.ImageField(upload_to='athletes/', null=True, blank=True)
+    height = models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Athlete: {self.username}"
+
 class Membership(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Team, on_delete=models.CASCADE)
     date_joined = models.DateField()
 
-class Publication(models.model):
+class Publication(models.Model):
     title = models.CharField(max_length=150)
     text = models.CharField(max_length=500)
     author = models.ForeignKey(Trainer, on_delete=models.CASCADE)
@@ -49,25 +50,25 @@ class Publication(models.model):
     def __str__(self):
         return self.title
 
-class MemberComment(models.model):
+class MemberComment(models.Model):
     text = models.CharField(max_length=500)
-    author = models.ForeignKey(member, on_delete=models.CASCADE)
+    author = models.ForeignKey(Member, on_delete=models.CASCADE)
     date_published = models.DateField()
     publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
-class AthleteComment(models.model):
+class AthleteComment(models.Model):
     text = models.CharField(max_length=500)
     author = models.ForeignKey(Athlete, on_delete=models.CASCADE)
     date_published = models.DateField()
     publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
-class TrainerComment(models.model):
+class TrainerComment(models.Model):
     text = models.CharField(max_length=500)
     author = models.ForeignKey(Trainer, on_delete=models.CASCADE)
     date_published = models.DateField()
     publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
-class Event(models.model):
+class Event(models.Model):
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
     created_by = models.ForeignKey(Trainer, on_delete=models.CASCADE)
@@ -83,22 +84,22 @@ class Training(Event):
     def __str__(self):
         return self.title
 
-class TrainingAttendance(models.model):
+class TrainingAttendance(models.Model):
     athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
     training = models.ForeignKey(Training, on_delete=models.CASCADE)
 
 class Game(Event):
     opponent = models.CharField(max_length=100)
     attendance = models.ManyToManyField(Member, through='GameAttendance')
-    participants = models.ManyToManyField(Athlete, through='GameAttendance')
+    participants = models.ManyToManyField(Athlete, through='GameParticipation')
 
     def __str__(self):
         return self.title
 
-class GameAttendance(models.model):
+class GameAttendance(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
-class GameParticipation(models.model):
+class GameParticipation(models.Model):
     athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
---
+++ SportTeamManagementTool/api/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('teams/', views.TeamList.as_view()),
+    path('teams/<int:pk>/', views.TeamDetail.as_view()),
+    path('teams/<int:pk>/publications/', views.TeamPublicationList.as_view()),
+    path('teams/<int:pk>/athletes/', views.TeamAthleteList.as_view()),
+    path('teams/<int:pk>/athletes/', views.TeamAthleteCreateView.as_view()), # added
+    path('teams/<int:pk>/members/', views.TeamMemberList.as_view()),
+    path('teams/<int:pk>/trainings/', views.TeamTrainingList.as_view()),
+    path('teams/<int:pk>/trainings/', views.TeamTrainingCreateView.as_view()), # Added
+    path('teams/<int:pk>/games/', views.TeamGameList.as_view()),
+    path('teams/<int:pk>/games/', views.TeamGameCreateView.as_view()), # Added
+    path('trainers/', views.TrainerList.as_view()),
+    path('trainers/<int:pk>/', views.TrainerDetail.as_view()),
+    path('publications/', views.PublicationList.as_view()),
+    path('publications/<int:pk>/', views.PublicationDetail.as_view()),
+    path('publications/<int:pk>/comments/', views.PublicationCommentList.as_view()),
+    path('comments/', views.CommentCreate.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
